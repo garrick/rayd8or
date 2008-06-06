06fbe83240cc90128d90eb94412f754094ac44e0
Garrick West <badlogin@Falken.local>
2008-06-06 13:13:07 -0700

    Removing profanity;)

--- lib/client/rayd8or.rb
+++ lib/client/rayd8or.rb
@@ -4,10 +4,8 @@ require 'shared/simple_http_sample.rb'
 class Rayd8or
 include DRbUndumped 
   def update(value) 
-    puts "value is a #{value.class}"
-    puts "fuck fuck fuck!"
-    puts "#{value[:title]} got #{value[:http_status]}"
-    #verse = "say #{value.text} returned #{value.code}"
-    #system("say #{value}")
+    verse =  "#{value[:title]} got #{value[:http_status]}"
+    puts "#{verse}"
+    system("say #{verse}")
   end 
 end
